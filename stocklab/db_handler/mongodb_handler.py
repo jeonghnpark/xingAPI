8be44a2ddcc7756b98635d8e6884b4a57332c8a0
jh70035 <jh70035@gmail.com>
2022-01-14 18:02:59 +0900

    mongodb2

--- stocklab/db_handler/mongodb_handler.py
+++ stocklab/db_handler/mongodb_handler.py
@@ -34,7 +34,66 @@ class MongoDBHandler:
             condition = {}
         if db_name is None or collection_name is None:
             raise Exception("need to param db_name, collection name")
-        return self._client[db_name][collection_name].find_one(condition, {"_id": False})
+        return self._client[db_name][collection_name].find_one(condition, {"_id": False})  # id는 빼고 리턴
+
+    def find_items(self, condition=None, db_name=None, collection_name=None):
+        if condition is None or not isinstance(condition, dict):
+            condition = {}  # no condition-> all data in db_collection
+        if db_name is None or collection_name is None:
+            raise Exception("Need to param db_name, db_collection")
+        return self._client[db_name][db_collection].find(condition, {"_id": False}, no_cursor_timeout=True,
+                                                         cursor_type=CursorType.EXHAUST)
+
+    def delete_items(self, condition=None, db_name=None, db_collection=None):
+        if condition is None or not isinstance(condition, dict):
+            raise Exception("need condition as a dict")
+
+        if db_name is None or db_collection is None:
+            raise Exception("need db_name, db_collection")
+
+        return self._client[db_name][db_collection].delete_many(condition)
+
+    def update_items(self, condition=None, update_value=None, db_name=None, db_collection=None):
+        if condition is None or not isinstance(condition, dict):
+            raise Exception("need condition as a dict")
+
+        if db_name is None or db_collection is None:
+            raise Exception("need db_name, db_collection")
+
+        return self._client[db_name][db_collection].update_many(filter=condition, update=update_value)
+
+    def update_item(self, condition=None, update_value=None, db_name=None, db_collection=None):
+        if condition is None or not isinstance(condition, dict):
+            raise Exception("need condition as a dict")
+
+        if db_name is None or db_collection is None:
+            raise Exception("need db_name, db_collection")
+
+        return self._client[db_name][db_collection].update_one(filter=condition, update=update_value)
+
+    def aggregate(self, pipeline=None, db_name=None, collection_name=None):
+        """
+        MongoDB의 aggregation 작업을 위한 메소드
+        :param pipeline:list: 갱신조건을 dict list로 받는다. [{},{},]
+        :param db_name:
+        :param collection_name:
+        :return: CommandCursor:obj: PyMongo의
+        """
+
+        if pipeline is None or not isinstance(pipeline, list):
+            raise Exception("need condition as a dict")
+        if db_name is None or db_collection is None:
+            raise Exception("need db_name, db_collection")
+        return self._client[db_name][collection_name].aggregate(pipeline)
+
+    def text_search(self, text=None, db_name=None, db_collection=None):
+        if text is None or not isinstance(text, str):
+            raise Exception("need text str")
+
+        if db_name is None or db_collection is None:
+            raise Exception("need db_name, db_collection")
+
+        return self._client[db_name][db_collection].find({"$text": {"$sear"}})
 
 
 if __name__ == "__main__":
